a16dbab044b0732b9f99754cd97bb17eb443a96b
iamkenjigido <i.am.kenji.gido@gmail.com>
2021-06-07 17:42:04 +0900

    modify question4again

--- artisdocument/questions/question4again.go
+++ artisdocument/questions/question4again.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // 洗濯乾燥機です
 type Machine struct {
@@ -71,23 +74,93 @@ func (c *Clothing) setClothingState(dirty, amount int) error {
 }
 
 
+// 洗濯機の洗濯の評価
+func (m *Machine) evaluateWash(cloth Clothing)  {
+	washTotalPower := m.washing.power * m.washing.time
+	clothDirt := cloth.level
+	if washTotalPower >= clothDirt {
+		fmt.Printf("洗浄が完了しました \n")
+	} else {
+		lackTime := math.Ceil(float64((washTotalPower - clothDirt) / m.washing.power * -1))
+		fmt.Printf("洗濯が完了するまで、もう%.f分お待ちください \n", lackTime)
+	}
+}
+
+// 乾燥機の乾燥の評価
+func (m *Machine) evaluateDry(cloth Clothing) {
+	dryTotalPower := m.drying.power * m.drying.time
+	clothAmount := cloth.volume
+	if dryTotalPower >= clothAmount {
+		fmt.Printf("乾燥が完了しました \n")
+	} else {
+		lackTime := math.Ceil(float64((dryTotalPower - clothAmount) / m.drying.power * -1))
+		fmt.Printf("完全に乾くまで、もう%.f分お待ちください \n", lackTime)
+	}
+}
+
 
 func main() {
+	fmt.Println("kenjiMachineを使う")
 	kenjiMachine := Machine{}
-	if err := kenjiMachine.setWashingStrength(10, 10); err != nil {
+	if err := kenjiMachine.setWashingStrength(4, 3); err != nil {
 		panic(err)
 	}
 	if err := kenjiMachine.setDryingStrength(8, 5); err != nil {
 		panic(err)
 	}
 
-	cloth := Clothing{}
 
+	cloth := Clothing{}
 	if err := cloth.setClothingState(4, 6); err != nil {
 		panic(err)
 	}
 
 
+	kenjiMachine.evaluateWash(cloth)
+	kenjiMachine.evaluateDry(cloth)
 
+	//fmt.Println("追加で洗濯したい時間を入力してください")
+	//var lackWashTime int
+	//fmt.Scan(&lackWashTime)
+	//kenjiMachine.setWashingStrength(4, lackWashTime)
+	//kenjiMachine.evaluateWash(cloth)
+
+	fmt.Println("kyokoMachineを使う")
+	kyokoMachine := Machine{}
+	if err := kyokoMachine.setWashingStrength(7, 10); err != nil {
+		panic(err)
+	}
+	if err := kyokoMachine.setDryingStrength(8, 10); err != nil {
+		panic(err)
+	}
+
+
+	cloth = Clothing{}
+	if err := cloth.setClothingState(7, 8); err != nil {
+		panic(err)
+	}
+
+
+	kyokoMachine.evaluateWash(cloth)
+	kyokoMachine.evaluateDry(cloth)
+
+	fmt.Println("kaitoMachineを使う")
+	kaitoMachine := Machine{}
+	if err := kaitoMachine.setWashingStrength(3, 10); err != nil {
+		panic(err)
+	}
+	if err := kaitoMachine.setDryingStrength(8, 10); err != nil {
+		panic(err)
+	}
+
+
+	cloth = Clothing{}
+	if err := cloth.setClothingState(4, 7); err != nil {
+		panic(err)
+	}
+
+
+	kaitoMachine.evaluateWash(cloth)
+	kaitoMachine.evaluateDry(cloth)
 
 }
